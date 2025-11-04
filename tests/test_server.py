f1199d491775d1bcde9e9cde7d6667a637689d2f
realqijun <ang.qijun2304@gmail.com>
2025-11-05 00:53:30 +0800

    Change import module in python test header

--- tests/test_server.py
+++ tests/test_server.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     server = GameNetSocket()
     server.bind(('localhost', 54321))
     server.connect(('127.0.0.1', 12345))
-    for i in range(100):
+    for i in range(1000):
         print(i, server.recv().decode(encoding='utf-8'), flush=True)
     server.close()
