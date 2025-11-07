8a6ca7e5457b49a954554ed9c780e50a94609e8a
meerkatboy <sohjunanerwin@gmail.com>
2025-11-07 16:52:12 +0800

    fix test cases

--- tests/test_client_r.py
+++ tests/test_client_r.py
@@ -3,7 +3,8 @@ from time import sleep, time
 
 client_addr = ("127.0.0.1", 4896)
 server_addr = ("127.0.0.1", 6767)
-tests = ["tests/test_cases/1.txt", "tests/test_cases/2.txt", "tests/test_cases/3.txt"]
+tests = ["tests/test_cases/1.txt", "tests/test_cases/2.txt", "tests/test_cases/3.txt", "tests/test_cases/4.txt"]
+# tests = ["tests/test_cases/3.txt"]
 
 def main():
     client = GameNetSocket()
@@ -15,28 +16,42 @@ def main():
     target_rate = 100
     interval = 1.0 / target_rate
 
-    # test empty data and single byte
-    client.send(b"", True)
+    # # test empty data and single byte
+    # client.send(b"", True)
     # client.send(b"a", True)
 
-    sleep(0.1)
-    # look for rtt print
+    # sleep(0.1)
+    # # look for rtt print
 
     chunk = 1024
-    packets_sent = 2  # already sent 2 packets above
+    packets_sent = 0  # already sent 2 packets above
 
     for file in tests:
         with open(file, "r") as f:
+
             data = f.read()
+
+            # while data:
+            #     next_time_to_send = time() + 1.0
+            #     for i in range(50):
+            #         if len(data) == 0:
+            #             break
+            #         client.send(data[:chunk].encode(), True)  # reliable send
+            #         data = data[chunk:]
+            #         packets_sent += 1
+
+            #     if time() < next_time_to_send:
+            #         sleep(next_time_to_send - time())
             next_time_to_send = time()
             while data:
-                current_time = time()
-                if current_time < next_time_to_send:
-                    sleep(next_time_to_send - current_time)
+                # current_time = time()
+                while time() < next_time_to_send:
+                    continue
+                # if current_time < next_time_to_send:
+                #     sleep(next_time_to_send - current_time)
                 client.send(data[:chunk].encode(), True)  # reliable send
                 data = data[chunk:]
                 packets_sent += 1
-                sleep(0.1)
                 next_time_to_send = time() + interval
 
     # all data sent
--- tests/test_server_r.py
+++ tests/test_server_r.py
@@ -1,8 +1,10 @@
 from api.gns import GameNetSocket
 from common import SocketTimeoutException
+from time import time
 
 server_addr = ("127.0.0.1", 6767)
-tests = ["tests/test_cases/1.txt", "tests/test_cases/2.txt", "tests/test_cases/3.txt"]
+# tests = ["tests/test_cases/3.txt"]
+tests = ["tests/test_cases/1.txt", "tests/test_cases/2.txt", "tests/test_cases/3.txt", "tests/test_cases/4.txt"]
 
 def main():
     sock = GameNetSocket()
@@ -10,22 +12,26 @@ def main():
     sock.listen()
     sock.accept()
 
+    start = time()
+
     # server accepted a connection
 
     try:
-        # receive 2 test packets first
-        data = sock.recv()
-        if not data:
-            raise Exception("Client disconnected before sending anyth")
-        data = sock.recv()
-        if not data:
-            raise Exception("Client disconnected before sending second message")
+        # # receive 2 test packets first
+        # data = sock.recv()
+        # if not data:
+        #     raise Exception("Client disconnected before sending anyth")
+        # data = sock.recv()
+        # if not data:
+        #     raise Exception("Client disconnected before sending second message")
 
-        received_packets = 2  # already received 2 packets above
+        received_packets = 0  # already received 2 packets above
         chunk = 1024
+        client_sent = 0
         for file in tests:
             with open(file, "r") as f:
                 expected_data = f.read()
+                client_sent += len(expected_data) / 1024
                 while expected_data:
                     packet = sock.recv()
                     if not packet:
@@ -36,14 +42,15 @@ def main():
                     expected_data = expected_data[chunk:]
                     received_packets += 1
 
-        print(f"Total packets received: {received_packets}")
-
     except SocketTimeoutException:
         print("Server timed out waiting for packet")
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
         sock.close()
+        print(f"Total packets received: {received_packets}")
+        print(f"Total packet rate: {received_packets / (time() - start):.2f} packets/sec")
+        print(f"Packet delivery ratio: {(received_packets / client_sent):.2%}")
         print("Server closed.")
 
 if __name__ == "__main__":
